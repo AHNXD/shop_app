81d2477c86c5b71590235c2155dadc8283caabf1
ayhamkefo <ayhamkefo048@gmail.com>
2024-12-30 16:31:00 +0300

    change url to sever

--- lib/constans.dart
+++ lib/constans.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 class Constans {
-  static const String kHost = "192.168.1.2";
-  static const ip = "192.168.1.12";
-  static const String kBaseUrl = "http://$ip:8000/api/";
-  // static const String kBaseUrl =
-  //     "https://62.72.13.145:8090/preview/dash.broker-sy.com/api/";
+  // static const String kHost = "192.168.1.2";
+  // static const ip = "192.168.1.12";
+  // static const String kBaseUrl = "http://$ip:8000/api/";
+  static const String kBaseUrl =
+      "https://62.72.13.145:8090/preview/dash.broker-sy.com/api/";
   static const String kImageBaseUrl = "${kBaseUrl}images?path=";
 
   static const String kFontFamily = 'Cairo';
